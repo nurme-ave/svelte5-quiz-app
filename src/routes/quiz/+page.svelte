6b7129b97ed5b34c3de4e0d8370aafab024e7d5d
Ave Nurme <nurme.ave@gmail.com>
2024-10-25 01:19:15 +0300

    Implement shuffling answers with Fisher-Yates algorithm instead of using Math.random()

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -54,11 +54,27 @@
 
 	let currentQuestion = $derived($questions[$currentQuestionIndex]);
 
+	function shuffleAnswers(array) {
+		let currentIndex = array.length;
+		let randomIndex;
+
+		// While there remain elements to shuffle
+		while (currentIndex > 0) {
+			// Pick a remaining element
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex--;
+
+			// And swap it with the current element
+			[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+		}
+
+		return array;
+	}
+
 	$effect(() => {
 		if (currentQuestion) {
-			shuffledAnswers = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer].sort(
-				() => Math.random() - 0.5
-			);
+			const answers = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer];
+			shuffledAnswers = shuffleAnswers(answers);
 		}
 	});
 
