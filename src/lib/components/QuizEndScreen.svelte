7dba7c4d1ed1694c05ac9f357534de97eab0d17b
Ave Nurme <nurme.ave@gmail.com>
2024-10-26 01:13:22 +0300

    Add a trophy icon, add a <div> to remove repetitive classes

--- src/lib/components/QuizEndScreen.svelte
+++ src/lib/components/QuizEndScreen.svelte
@@ -17,18 +17,20 @@
 </script>
 
 <div class="text-white text-center md:w-[32rem] fade-in-from-top delay-1 space-y-5">
-	<h1 class="text-2xl md:text-3xl font-bold mb-4 text-yellow-300">Congratulations!</h1>
-	<p>You've completed the quiz!</p>
-	<p>Your score: {$score} / {totalQuestions}</p>
-	<p class="balanced-text">
-		{#if $score === totalQuestions}
-			Perfect score! You're a quiz&nbsp;master!
-		{:else if $score > totalQuestions / 2}
-			Great job! You did well!
-		{:else}
-			Nice try! There's room for improvement.
-		{/if}
-	</p>
+	<h1 class="text-2xl md:text-3xl font-bold mb-4">Congratulations!</h1>
+	<div class="md:text-lg">
+		<p>You've completed the quiz! <i class="fa-solid fa-trophy text-yellow-300"></i></p>
+		<p>Your score: {$score} / {totalQuestions}</p>
+		<p class="balanced-text">
+			{#if $score === totalQuestions}
+				Perfect score! You're a quiz&nbsp;master!
+			{:else if $score > totalQuestions / 2}
+				Great job! You did well!
+			{:else}
+				Nice try! There's room for improvement.
+			{/if}
+		</p>
+	</div>
 	<Button onclick={restartQuiz} variant="primary" customClass="w-44 text-lg font-semibold mx-auto">
 		Start New Quiz
 	</Button>
