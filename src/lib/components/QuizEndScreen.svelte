5698d73dcff303f2c672d9e52a8efc2c42855af3
Ave Nurme <nurme.ave@gmail.com>
2024-10-26 01:00:45 +0300

    Work on QuizEndScreen styling

--- src/lib/components/QuizEndScreen.svelte
+++ src/lib/components/QuizEndScreen.svelte
@@ -16,11 +16,11 @@
 	let totalQuestions = $derived($questions.length);
 </script>
 
-<div class="text-white text-center md:w-[32rem] fade-in-from-top delay-1">
+<div class="text-white text-center md:w-[32rem] fade-in-from-top delay-1 space-y-5">
 	<h1 class="text-2xl md:text-3xl font-bold mb-4 text-yellow-300">Congratulations!</h1>
-	<p class="text-xl mb-4">You've completed the quiz!</p>
-	<p class="text-2xl mb-6">Your score: {$score} / {totalQuestions}</p>
-	<p class="mb-8">
+	<p>You've completed the quiz!</p>
+	<p>Your score: {$score} / {totalQuestions}</p>
+	<p>
 		{#if $score === totalQuestions}
 			Perfect score! You're a quiz master!
 		{:else if $score > totalQuestions / 2}
@@ -29,11 +29,7 @@
 			Nice try! There's room for improvement.
 		{/if}
 	</p>
-	<Button
-		onclick={restartQuiz}
-		variant="primary"
-		customClass="w-36 mt-3 text-lg font-semibold mx-auto"
-	>
+	<Button onclick={restartQuiz} variant="primary" customClass="w-44 text-lg font-semibold mx-auto">
 		Start New Quiz
 	</Button>
 </div>
